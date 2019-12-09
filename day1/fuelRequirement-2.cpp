ef463c5d31c3315594c3617b0c9027e29b6cb110
AxelCurso <bessyaxel69@gmail.com>
2019-12-09 12:39:48 +0100

    day1

---
+++ day1/fuelRequirement-2.cpp
@@ -0,0 +1,83 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct  Data{
+    int     *d;
+    int     *afterCalc;
+    int     len;
+};
+
+void    getData(char *file, Data &data){
+    FILE    *fptr;
+    char    c;
+    int     i;
+    bool    inNum;
+
+    if ((fptr = fopen(file, "r")) == NULL){
+       printf("Error! Can open the file just one time");
+       exit(1);
+    }
+    data.len = 0;
+    while ((c = fgetc(fptr)) != EOF){
+        if (c == '\n'){
+            data.len++;
+        }
+    }
+    data.d = (int*)malloc(sizeof(int) * data.len);
+    data.afterCalc = (int*)malloc(sizeof(int) * data.len);
+    fseek(fptr, 0, SEEK_SET);
+    i = 0;
+    inNum = false;
+    while ((c = fgetc(fptr)) != EOF){
+        if (c == '\n'){
+            i++;
+            inNum = false;
+        } else if (!inNum){
+            data.d[i] = c - '0';
+            inNum = true;
+        } else {
+            data.d[i] *= 10;
+            data.d[i] += c - '0';
+        }
+    }
+    fclose(fptr);
+}
+
+int     values(int val){
+    if (val /3-2< 1){
+        return (0);
+    }
+    return (values(val / 3 - 2) + (val / 3 - 2));
+}
+
+void    getValues(Data &data){
+    int     i;
+
+    for (i = 0; i < data.len; i++){
+        data.afterCalc[i] = values(data.d[i]);
+    }
+}
+
+int     getFuel(Data &data){
+    int     i;
+    int     value;
+
+    value = 0;
+    for (i = 0; i < data.len; i++){
+        value += data.afterCalc[i];
+    }
+    return (value);
+}
+
+int     main(int ac, char **av){
+    Data    data;
+
+    if (ac != 2){
+        printf("Usage: ./a.out file.txt (fill contains the values for the calculation of the fuel)\n");
+        exit(1);
+    }
+    getData(av[1], data);
+    getValues(data);
+    printf("%i\n", getFuel(data));
+    return (0);
+}
